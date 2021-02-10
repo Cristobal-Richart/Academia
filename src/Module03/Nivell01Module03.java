e179ba628e766e079c37442e8942be1372548892
Hristo69 <buscarselavida@gmail.com>
2021-02-10 11:01:32 +0100

    Modulo0302

--- src/Module03/Nivell01Module03.java
+++ src/Module03/Nivell01Module03.java
@@ -1,7 +1,11 @@
 package Module03;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 public class Nivell01Module03 {
 public static void main(String[] args){
+	
+	// Fase 01
 	
 	String ciutat01="";
 	String ciutat02="";
@@ -19,9 +23,28 @@ public static void main(String[] args){
 		ciutat05=entrada.next();
 		ciutat06=entrada.next();
 		
-	System.out.println(ciutat01+"\n"+ciutat02+"\n"+ciutat03+"\n"+ciutat04+"\n"+ciutat05+"\n"+ciutat06);
+	//System.out.println(ciutat01+"\n"+ciutat02+"\n"+ciutat03+"\n"+ciutat04+"\n"+ciutat05+"\n"+ciutat06);
 		
 		entrada.close();
+	
+	// Fase 02
+		
+	ArrayList<String> ciudades= new ArrayList<String>();
+	
+		ciudades.add(ciutat01);
+		ciudades.add(ciutat02);
+		ciudades.add(ciutat03);
+		ciudades.add(ciutat04);
+		ciudades.add(ciutat05);
+		ciudades.add(ciutat06);
+		
+		Collections.sort(ciudades,String.CASE_INSENSITIVE_ORDER);		
+		
+		for(String sElemento: ciudades){
+		 System.out.println(sElemento);	
+		}
+		
+		
+	
 }
-
 }
