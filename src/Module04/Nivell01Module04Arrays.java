79875a63ae766dfa6f69ea4f0b7c633f2a9a962e
Hristo69 <buscarselavida@gmail.com>
2021-02-18 12:28:43 +0100

    Mo4

--- src/Module04/Nivell01Module04Arrays.java
+++ src/Module04/Nivell01Module04Arrays.java
@@ -1,7 +1,5 @@
 package Module04;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 public class Nivell01Module04Arrays {
 public static void main(String[] args) {
 	
@@ -59,10 +57,78 @@ public static void main(String[] args) {
 		
 	
 	
-	System.out.println();System.out.println("¿Que desea comer?");
+	
+	
 	
 
+	ArrayList<Integer> pedido= new ArrayList<Integer>();
 	
 	
+	int numeroplato = 0;
+	int salir=9;
+	System.out.println();
+	System.out.println("¿Que desea pedir para comer?, pulse 9 para salir del menú.");
+	while(numeroplato!=salir) {
+		
+		Scanner entrada=new Scanner (System.in);
+		numeroplato=entrada.nextInt();
+		pedido.add(numeroplato);
+		if(numeroplato==1) {
+			System.out.println("Has escogido Carrillada de cerdo Ibérico.");
+			System.out.println("¿Deseas algo más?");
+			
+		}else if(numeroplato==2) {
+			System.out.println("Has escogido Verduras a la Plancha.");
+			System.out.println("¿Deseas algo más?");
+		}
+		else if(numeroplato==3) {
+			System.out.println("Has escogido Gazpacho Manchego.");
+			System.out.println("¿Deseas algo más?");
+		}
+		else if(numeroplato==4) {
+			System.out.println("Has escogido Paella.");
+			System.out.println("¿Deseas algo más?");
+		}
+				
+		else System.out.println("La comanda ha sido pasada a cocina, Gracias.");
+		System.out.println();
+	}
+	
+	for (int i=0; i<pedido.size();i++) {
+		
+		if(i==1) System.out.println(plato[0]+" "+precio[0]);
+		if(i==2) System.out.println(plato[1]+" "+precio[1]);
+		if(i==3) System.out.println(plato[2]+" "+precio[2]);
+		if(i==4) System.out.println(plato[3]+" "+precio[3]);
+		
+		
+		
+	}
+	int []suma=new int [precioplato.size()];
+	for (int i=0; i<precioplato.size();i++) {
+		suma[i]=precioplato(i);
+		System.out.println(i);
+		
+	}
+	
+	
+	
+	
+	
+		
+	
+	
+	System.out.println("Pase por caja y difrute de su menu.");
 }
+
+private static int precioplato(int i) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
+private static int pedido(int i) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+
 }
